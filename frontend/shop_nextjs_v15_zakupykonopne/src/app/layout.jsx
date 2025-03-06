f9fc58fe280e4c67215d3473a1b79485ec700354
TDomagalski <tomekwebdev@gmail.comL>
2025-03-06 11:37:39 +0100

    fix

---
+++ frontend/shop_nextjs_v15_zakupykonopne/src/app/layout.jsx
@@ -0,0 +1,27 @@
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.scss';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata = {
+  title: 'ZakupyKonopne.pl',
+  description: 'Sklep z wyrobami z konopi włóknistej',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
